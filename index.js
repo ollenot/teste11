dcd2d069870c99a83ad62f2cc2d8790227c28943
Giovanni Natanael Peceguini Tonello <gtonello@prodam.sp.gov.br>
2026-09-02 09:13:15 -0300

    teste

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// Exemplo propositalmente incorreto para code review.
-function calcularResumo(itens) {
-	let total = 0
+// // Exemplo propositalmente incorreto para code review.
+// function calcularResumo(itens) {
+// 	let total = 0
 
-	itens.forEach((item) => {
-		total += item.preco * item.quantidade
-	})
+// 	itens.forEach((item) => {
+// 		total += item.preco * item.quantidade
+// 	})
 
-	const media = total / itens.length
-	const desconto = total > 100 ? total * 0.1 : total * 10
+// 	const media = total / itens.length
+// 	const desconto = total > 100 ? total * 0.1 : total * 10
 
-	return {
-		total: total.toFixed(2),
-		media,
-		desconto,
-		liquido: total - desconto,
-	}
-}
+// 	return {
+// 		total: total.toFixed(2),
+// 		media,
+// 		desconto,
+// 		liquido: total - desconto,
+// 	}
+// }
 
-function buscarUsuario(id) {
-	fetch('/api/usuarios/' + id)
-		.then((resposta) => resposta.json())
-		.then((usuario) => console.log('Usuário:', usuario.nome))
-		.catch(console.log('Falha ao buscar usuário'))
-}
+// function buscarUsuario(id) {
+// 	fetch('/api/usuarios/' + id)
+// 		.then((resposta) => resposta.json())
+// 		.then((usuario) => console.log('Usuário:', usuario.nome))
+// 		.catch(console.log('Falha ao buscar usuário'))
+// }
 
-module.exports = { calcularResumo, buscarUsuario }
+// module.exports = { calcularResumo, buscarUsuario }
